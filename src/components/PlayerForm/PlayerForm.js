6caf386567b97db87edb12affa2556a074f7244a
Ben <benjamin.main88@gmail.com>
2020-12-20 12:28:08 +0000

    Local state updates with name value from input field

--- src/components/PlayerForm/PlayerForm.js
+++ src/components/PlayerForm/PlayerForm.js
@@ -7,6 +7,12 @@ class PlayerForm extends React.Component {
         this.state = {
             name: "",
         };
+
+        this.handleName = this.handleName.bind(this);
+    }
+
+    handleName(event) {
+        this.setState({ name: event.currentTarget.value });
     }
 
     render() {
@@ -15,7 +21,8 @@ class PlayerForm extends React.Component {
                 <h1>Add Player</h1>
                 <form>
                     <label>Name</label>
-                    <input></input>
+                    <input onChange={this.handleName}></input>
+                    <button>Add Player</button>
                 </form>
             </>
         );
