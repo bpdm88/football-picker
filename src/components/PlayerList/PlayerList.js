55bef58fffd7cfd0db3a6c7552e046d7835574a9
Ben <benjamin.main88@gmail.com>
2020-12-22 16:51:39 +0000

    Modified player list so that skill scores display within spans so can be formatted later

--- src/components/PlayerList/PlayerList.js
+++ src/components/PlayerList/PlayerList.js
@@ -2,7 +2,12 @@ const PlayerList = ({ players }) => {
     return (
         <ul>
             {players.map((player) => (
-                <li key={player.id}>{player.name}</li>
+                <li key={player.id}>
+                    <span>{player.name}</span>
+                    <span>{player.attack}</span>
+                    <span>{player.defence}</span>
+                    <span>{player.creativity}</span>
+                </li>
             ))}
         </ul>
     );
