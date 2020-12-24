878092c2ca841139d1409daa295cdea0722d3d0f
Ben <benjamin.main88@gmail.com>
2020-12-24 18:25:55 +0000

    match day info is rendered on click

--- src/components/MatchDay/MatchDay.js
+++ src/components/MatchDay/MatchDay.js
@@ -12,9 +12,15 @@ class MatchDay extends React.Component {
     }
 
     render() {
+        let { matchInfo } = this.props;
         return (
             <section>
                 <button onClick={this.handleClick}>Match Day Info</button>
+                <ul>
+                    {matchInfo.map((detail) => (
+                        <li key={detail.id}>{detail}</li>
+                    ))}
+                </ul>
             </section>
         );
     }
--- src/components/MatchDay/index.js
+++ src/components/MatchDay/index.js
@@ -2,10 +2,16 @@ import { connect } from "react-redux";
 import MatchDay from "./MatchDay";
 import { getInfo } from "../../data/actions/state";
 
+let mapStateToProps = (state) => {
+    return {
+        matchInfo: state.matchInfo,
+    };
+};
+
 let mapDispatchToProps = (dispatch) => {
     return {
         handleMatchDay: () => dispatch(getInfo()),
     };
 };
 
-export default connect(null, mapDispatchToProps)(MatchDay);
+export default connect(mapStateToProps, mapDispatchToProps)(MatchDay);
