115bf8fc6d5e69e12a55e3cd7e50799b6e0c8918
Ben <benjamin.main88@gmail.com>
2020-12-31 14:38:09 +0000

    Available squad layout responsive

--- src/components/PlayerList/PlayerList.js
+++ src/components/PlayerList/PlayerList.js
@@ -2,13 +2,33 @@ const PlayerList = ({ players }) => {
     return (
         <section className="large-card">
             <h2 className="heading-bravo">Available Squad</h2>
+            <div class="abilities">
+                <h4 className="heading-charlie">Player</h4>
+                <div className="scores">
+                    <h4 className="heading-charlie">Attack</h4>
+                    <h4 className="heading-charlie">Defend</h4>
+                    <h4 className="heading-charlie">Creativity</h4>
+                </div>
+            </div>
             <ul>
                 {players.map((player) => (
                     <li key={player.id}>
-                        <span>{player.name}</span>
-                        <span>{player.attack}</span>
-                        <span>{player.defence}</span>
-                        <span>{player.creativity}</span>
+                        <div class="abilities">
+                            <span className="heading-charlie">
+                                {player.name}
+                            </span>
+                            <div className="scores">
+                                <span className="heading-charlie">
+                                    {player.attack}
+                                </span>
+                                <span className="heading-charlie">
+                                    {player.defence}
+                                </span>
+                                <span className="heading-charlie">
+                                    {player.creativity}
+                                </span>
+                            </div>
+                        </div>
                     </li>
                 ))}
             </ul>
