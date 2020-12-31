2d2cc534daca1ccb484de82e0c5a033810a5d446
Ben <benjamin.main88@gmail.com>
2020-12-31 22:29:38 +0000

    Final Score card - correct styling and layout

--- src/components/FinalScore/FinalScore.js
+++ src/components/FinalScore/FinalScore.js
@@ -3,10 +3,14 @@ const FinalScore = ({ teamName1, teamName2, team1Goals, team2Goals }) => {
         <section className="small-card">
             <h2 className="heading-bravo">Final Score</h2>
             <div className="content">
-                <h1>{teamName1}</h1>
-                <h2>{team1Goals}</h2>
-                <h1>{teamName2}</h1>
-                <h2>{team2Goals}</h2>
+                <h1 className="heading-echo">
+                    {teamName1}
+                    <span className="goals">{team1Goals}</span>
+                </h1>
+                <h1 className="heading-echo">
+                    {teamName2}
+                    <span className="goals">{team2Goals}</span>
+                </h1>
             </div>
         </section>
     );
