631378f838ba8c4f85ed43a8a186f7e96c05f30a
Ben <benjamin.main88@gmail.com>
2021-01-02 11:37:23 +0000

    Added content class to card so that button is positioned correctly

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -39,22 +39,24 @@ class Team extends React.Component {
             <section className="small-card">
                 <h2 className="heading-bravo">Create Teams</h2>
                 <form onSubmit={this.handleSubmit}>
-                    <FormField
-                        label="Team Name 1:"
-                        name="team-name"
-                        type="text"
-                        handleChange={this.handleChangeTeamName1}
-                        value={teamOne}
-                        className="label-block"
-                    />
-                    <FormField
-                        label="Team Name 2:"
-                        name="team-name"
-                        type="text"
-                        handleChange={this.handleChangeTeamName2}
-                        value={teamTwo}
-                        className="label-block"
-                    />
+                    <div className="content">
+                        <FormField
+                            label="Team Name 1:"
+                            name="team-name"
+                            type="text"
+                            handleChange={this.handleChangeTeamName1}
+                            value={teamOne}
+                            className="label-block"
+                        />
+                        <FormField
+                            label="Team Name 2:"
+                            name="team-name"
+                            type="text"
+                            handleChange={this.handleChangeTeamName2}
+                            value={teamTwo}
+                            className="label-block"
+                        />
+                    </div>
                     <button disabled={disabled} className="primary-button">
                         Add Teams
                     </button>
